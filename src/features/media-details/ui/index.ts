5e238f845a9b1445b4136c03872f1a508a72f59c
wayme <romka2001ivaniuk@gmail.com>
2025-12-28 19:44:03 +0200

    refactor: update nextjs, remove redundunt packages

--- src/features/media-details/ui/index.ts
+++
@@ -1,6 +0,0 @@
-export * from './media-info';
-export * from './user-activity';
-export * from './media-status-stats';
-export * from './media-rate-stats';
-export * from './comments';
-export * from './media-trailer';
